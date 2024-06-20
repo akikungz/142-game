4416fe62f5ea2296cdf852b992a27242490e365f
Jangecraft <78905953+Jangecraft@users.noreply.github.com>
2024-06-20 16:53:21 +0700

    เพิ่ม framework สำหรับ หน้าจอและปุ่ม

---
+++ framework.py
@@ -0,0 +1,113 @@
+import pygame
+from math import ceil
+
+
+class Screen():
+    def __init__(self, x, y):
+        # กำหนดการแบ่งหน้าจอ
+        self.MAX_X = x
+        self.MAX_Y = y
+        # ตั้งค่าให้เต็มจอเป็นค่าเริ่มต้น
+        self.set_fullscreen_mode()
+
+    def set_fullscreen_mode(self):
+        # ตั้งค่าโหมดการแสดงผลเป็นเต็มหน้าจอ
+        self.window = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        # ตรวจจับความละเอียดหน้าจอปัจจุบัน
+        self.__get_screen_info()
+        # แบ่งหน้าจออกเป็น grid
+        self.__set_axis()
+
+    def set_screen(self, width, height):
+        # ตั้งค่าโหมดการแสดงผลเป็นค่าที่กำหนด
+        self.window = pygame.display.set_mode((width, height))
+        # ตรวจจับความละเอียดหน้าจอปัจจุบัน
+        self.__get_screen_info()
+        # แบ่งหน้าจออกเป็น grid
+        self.__set_axis()
+
+    def pack_x(self, box_x: int) -> int:
+        return self.__resize2x(box_x)
+    
+    def pack_y(self, box_y: int) -> int:
+        return self.__resize2y(box_y)
+    
+    def width(self, scale: int) -> int:
+        return self.__resize2x(scale)
+    
+    def height(self, scale: int) -> int:
+        return self.__resize2y(scale)
+    
+    def __resize2x(self, scale: int):
+        scale = min(self.MAX_X, scale)
+        scale = max(scale, 0)
+        return ceil(self.x_axis * scale)
+    
+    def __resize2y(self, scale: int):
+        scale = min(self.MAX_Y, scale)
+        scale = max(scale, 0)
+        return ceil(self.y_axis * scale)
+
+    def __set_axis(self):
+        self.x_axis = self.SCREEN_WIDTH / self.MAX_X
+        self.y_axis = self.SCREEN_HEIGHT / self.MAX_Y
+
+    def __get_screen_info(self):
+        screen_info = pygame.display.Info()
+        self.SCREEN_WIDTH = screen_info.current_w
+        self.SCREEN_HEIGHT = screen_info.current_h
+
+
+class Button():
+    def __init__(self, 
+                 text: str, 
+                 size_text: int, 
+                 color_text: tuple, 
+                 color_button: tuple, 
+                 radius=20, 
+                 font=None):
+        self.text = text
+        self.font = pygame.font.SysFont(font, size_text) if font else pygame.font.SysFont(None, size_text)
+        self.text_surface = self.font.render(text, True, color_text)
+        self.set_button()
+        self.color_button = color_button
+        self.color_text = color_text
+        self.radius = radius
+        # สถานะทั้งหมดได้แก่ "normal", "hover", "pressed"
+        self.state = "normal"  
+
+    def show(self, 
+             screen_draw: pygame.Surface, 
+             width_button: int, 
+             height_button: int, 
+             x: int, y: int):
+        self.set_button(width_button, height_button, x, y)
+        button_color = self.color_button
+        text_color = self.color_text
+        
+        # เมื่อเมาส์ลอยอยู่เหนือปุ่ม
+        if self.state == "hover":
+            button_color = [min(255, c + 120) for c in self.color_button] # ทำให้ปุ่มสว่างขึ้น
+            text_color = [min(255, c + 20) for c in self.color_text]
+        
+        pygame.draw.rect(screen_draw, button_color, self.button, 0, self.radius)
+        screen_draw.blit(self.font.render(self.text, True, text_color), self.text_rect)
+
+    def set_button(self, width_button=1, height_button=1, x=1, y=1):
+        self.text_rect = self.text_surface.get_rect(center=(x+(width_button/2), y+(height_button/2)))
+        self.button = pygame.Rect(x, y, width_button, height_button)
+    
+    def click(self, event):
+        if event.type == pygame.MOUSEMOTION:
+            if self.button.collidepoint(event.pos):
+                self.state = "hover"
+            else:
+                self.state = "normal"
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if self.button.collidepoint(event.pos):
+                self.state = "pressed"
+                return True  # ส่งคืนค่าว่ากดปุ่มแล้ว
+        elif event.type == pygame.MOUSEBUTTONUP:
+            if self.state == "pressed":
+                self.state = "normal"
+        return False
--- main.py
+++ main.py
@@ -1,4 +1,108 @@
+import sys
 import pygame
 import random
 import get_image
 import get_audio
+import framework as fw
+
+
+# Initialize Pygame
+pygame.init()
+
+
+# Colors
+WHITE = (255, 255, 255)
+GREY = (198, 198, 198)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+DARK_BLUE = (0, 0, 139)
+
+
+class Variable():
+    def __init__(self):
+        self.clock = pygame.time.Clock()
+        # ตัวแปรของปุ่ม
+        self.btnPlay = fw.Button('play', 20, WHITE, DARK_BLUE)
+        self.btnSetting = fw.Button('setting', 20, WHITE, DARK_BLUE)
+        self.btnExit = fw.Button('exit', 20, WHITE, RED)
+        self.btnFullscreen = fw.Button('Fullscreen', 20, WHITE, DARK_BLUE)
+        self.btn1080P = fw.Button('1920x1080', 20, WHITE, DARK_BLUE)
+        self.btn720P = fw.Button('1280x720', 20, WHITE, DARK_BLUE)
+        self.btn480P = fw.Button('854x480', 20, WHITE, DARK_BLUE)
+        self.btn360P = fw.Button('640x360', 20, WHITE, DARK_BLUE)
+        # set ค่าเริ่มต้น
+        self.set_start()
+
+    def set_start(self):
+        pass
+
+
+# global variable
+screen = fw.Screen(640, 360)
+var = Variable()
+
+
+while True:
+    # ตัวแปรสำหรับเข้าแต่ละหน้า
+    page_play = False
+    page_setting = False
+    # ตัวแปลอีเว้น
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:  # Press ESC to exit
+                pygame.quit()
+                sys.exit()
+        elif var.btnPlay.click(event):
+            page_play = True
+            var.set_start()
+        elif var.btnSetting.click(event):
+            page_setting = True
+        elif var.btnExit.click(event):
+            pygame.quit()
+            sys.exit()
+    
+    screen.window.fill(WHITE)
+    var.btnPlay.show(screen.window, screen.width(160), screen.height(20), screen.pack_x(240), screen.pack_y(150))
+    var.btnSetting.show(screen.window, screen.width(160), screen.height(20), screen.pack_x(240), screen.pack_y(180))
+    var.btnExit.show(screen.window, screen.width(160), screen.height(20), screen.pack_x(240), screen.pack_y(210))
+    pygame.display.flip()
+    var.clock.tick(30)
+
+    while page_setting:
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:  # Press ESC to exit
+                    pygame.quit()
+                    sys.exit()
+            elif var.btnExit.click(event):
+                page_setting = False
+            elif var.btnFullscreen.click(event):
+                screen.set_fullscreen_mode()
+            elif var.btn1080P.click(event):
+                screen.set_screen(1920, 1080)
+            elif var.btn720P.click(event):
+                screen.set_screen(1280, 720)
+            elif var.btn480P.click(event):
+                screen.set_screen(854, 480)
+            elif var.btn360P.click(event):
+                screen.set_screen(640, 360)
+
+        screen.window.fill(WHITE)
+        var.btnFullscreen.show(screen.window, screen.width(160), screen.height(20), screen.pack_x(240), screen.pack_y(100))
+        var.btn1080P.show(screen.window, screen.width(160), screen.height(20), screen.pack_x(240), screen.pack_y(130))
+        var.btn720P.show(screen.window, screen.width(160), screen.height(20), screen.pack_x(240), screen.pack_y(160))
+        var.btn480P.show(screen.window, screen.width(160), screen.height(20), screen.pack_x(240), screen.pack_y(190))
+        var.btn360P.show(screen.window, screen.width(160), screen.height(20), screen.pack_x(240), screen.pack_y(220))
+        var.btnExit.show(screen.window, screen.width(100), screen.height(20), screen.pack_x(520), screen.pack_y(10))
+
+        pygame.display.flip()
+        var.clock.tick(30)
