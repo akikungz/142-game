075a4c7c8702e007fc12c4e78b6876036a4a03f2
Jangecraft <78905953+Jangecraft@users.noreply.github.com>
2024-06-25 14:08:08 +0700

    จัดระเบียบตัวแปร

--- variable.py
+++ variable.py
@@ -6,10 +6,8 @@ import framework as fw
 
 class Variable():
     def __init__(self, pygame: pygame):
+        # ตัวแปรใช้ทั่วไป
         self.clock = pygame.time.Clock()
-        # ตัวแปร
-        self.result = 'Random Now!!'
-        self.count_gacha = 0
         # Colors
         self.colors = colors.Colors()
         # ตัวแปรของเพลง
@@ -18,9 +16,12 @@ class Variable():
         self.audio_gacha = pygame.mixer.Sound(get_audio.gacha_sound)
         self.set_audio_volume()
         self.audio_background_music.play(-1)
+        # -------------------------------------
+
+        # ตัวแปรหน้า page_main
         # ตัวแปรข้อความ
         self.text_name_game = fw.Text('142 Game', 50, self.colors.BLACK)
-        self.text_normal = fw.Text('', 30, self.colors.BLACK)
+        self.text_version = fw.Text('', 30, self.colors.BLACK)
         # ตัวแปรของปุ่ม
         self.btnPlay = fw.Button('play', 20, self.colors.WHITE, self.colors.DARK_BLUE)
         self.btnGacha = fw.Button('gacha', 20, self.colors.WHITE, self.colors.DARK_BLUE)
@@ -28,13 +29,26 @@ class Variable():
         self.btnExit = fw.Button('exit', 20, self.colors.WHITE, self.colors.RED)
         self.btnPrevious = fw.Button('<', 20, self.colors.WHITE, self.colors.DARK_BLUE)
         self.btnNext = fw.Button('>', 20, self.colors.WHITE, self.colors.DARK_BLUE)
+        # -------------------------------------
+
+        # ตัวแปรหน้า page_setting
+        # ตัวแปรของปุ่ม
         self.btnReduce_1 = fw.Button('-1', 20, self.colors.WHITE, self.colors.DARK_BLUE)
         self.btnIncrease_1 = fw.Button('+1', 20, self.colors.WHITE, self.colors.DARK_BLUE)
         self.btnReduce_10 = fw.Button('-10', 20, self.colors.WHITE, self.colors.DARK_BLUE)
         self.btnIncrease_10 = fw.Button('+10', 20, self.colors.WHITE, self.colors.DARK_BLUE)
-        self.btnRandom = fw.Button('random', 20, self.colors.WHITE, self.colors.GOLD)
         # ตัวแปรของ dropdown
         self.dropdownScreen = fw.Dropdown(['Full Screen', '1920x1080', '1280x720', '854x480'], 24, self.colors.WHITE, self.colors.DARK_BLUE)
+        # -------------------------------------
+
+        # ตัวแปรหน้า page_gacha
+        # ตัวแปรข้อความ
+        self.result = 'Random Now!!'
+        self.count_gacha = 0
+        # ตัวแปรของปุ่ม
+        self.btnRandom = fw.Button('random', 20, self.colors.WHITE, self.colors.GOLD)
+        # -------------------------------------
+
         # set ค่าเริ่มต้น
         self.set_start()
 
